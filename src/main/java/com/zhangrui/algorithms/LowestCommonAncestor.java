530700ed0430ed4fbaf3e15a1bc21807b76f332b
zhangrui <1037102215@qq.com>
2026-05-22 10:47:13 +0800

    lowestCommonAncestor

--- src/main/java/com/zhangrui/algorithms/LowestCommonAncestor.java
+++ src/main/java/com/zhangrui/algorithms/LowestCommonAncestor.java
@@ -10,8 +10,21 @@ public class LowestCommonAncestor {
 
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        //左子树
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
 
-        return null;
+        //右子树
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+
+        return left == null ? right : left;
     }
 
 }
